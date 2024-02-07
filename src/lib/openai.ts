a012d7a6f987fe714b87b9d9fdfa74fded146034
sanket95droid <sanketzambre1@gmail.com>
2024-02-07 15:51:01 +0530

    understanding

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -12,10 +12,12 @@ export async function generateImagePrompt(name: string) {
             model: 'gpt-3/5-turbo',
             messages: [
                 {
+                    // telling gpt his role
                     role: 'system',
                     content: 'you are a creative and helpful AI assistant capable of generating interesting thumbnail descriptions for my notes. Your output will be fetched into the DALL-E API to genereate a thumbnail. I want the description to be minimalistic and flat style.'
                 },
                 {
+                    // giving gpt the note name to generate the prompt for thumbnail
                     role: 'user',
                     content: `please genereate a thumbnail description for my notebook titles ${name}`
                 }
