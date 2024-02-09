c6232aa91c070a750b10f2fc2efffc4a2b04bbc0
sanket95droid <sanketzambre1@gmail.com>
2024-02-09 16:49:27 +0530

    updated-favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import {ClerkProvider} from '@clerk/nextjs'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "📝 Pro Note",
+  title: "Pro Note",
 };
 
 export default function RootLayout({
