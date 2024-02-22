82521d378a874eb46271b7b5d3b1360b096dc93f
sanket95droid <sanketzambre1@gmail.com>
2024-02-23 02:47:24 +0530

    ((*_*))

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -20,8 +20,8 @@ const PricingPage = () => {
             <Navbar />
             <div className="absolute flex justify-center items-center -translate-y-1/2 max-[950px]:-translate-y-[20%] -translate-x-1/2 left-1/2 top-1/2">
                 <div className="grid max-[950px]:grid-cols-1 max-[1200px]:gap-32 max-[950px]:gap-8 grid-cols-3 gap-8">
-                    <div className="hover:scale-110 p-8 border text-center justify-self-center duration-200 backdrop-filter backdrop-blur-md bg-opacity-0 bg-white/0 border-gray-100/15 outline-none shadow-xl rounded-xl">
-                        <h2 className="text-4xl text-red-400 drop-shadow-glow font-bold line-through">$7</h2>
+                    <div className="hover:scale-110 cursor-pointer p-8 border text-center justify-self-center duration-200 backdrop-filter backdrop-blur-md bg-opacity-0 bg-white/0 border-gray-100/15 outline-none shadow-xl rounded-xl">
+                        <h2 className="text-4xl text-white drop-shadow-glow font-bold line-through">$7</h2>
                         <p className="text-white text-[22px]">FREE</p>
                         <ul className="mt-6 text-white/75">
                             <li>Feature 1</li>
@@ -30,8 +30,9 @@ const PricingPage = () => {
                         </ul>
                         <Button className="mt-2 hover:scale-110 transition ease-in-out delay-75 hover:drop-shadow-glow bg-[#bd0e32] text-white font-semibold rounded-[8px] hover:bg-white hover:text-[#222831] duration-200 p-3">Choose Plan</Button>
                     </div>
-                    <div className="hover:scale-110 p-8 border text-center justify-self-center duration-200 backdrop-filter backdrop-blur-md bg-opacity-0 bg-white/0 border-gray-100/15 outline-none shadow-xl rounded-xl">
-                        <h2 className="text-4xl text-white font-bold">$29</h2>
+                    <div className="hover:scale-125 scale-110 cursor-pointer p-8 border text-center justify-self-center duration-200 backdrop-filter backdrop-blur-md bg-opacity-0 bg-white/0 border-gray-100/15 outline-none shadow-xl rounded-xl">
+                        <p className="rounded-2xl bg-lime-300 top-1 text-xs w-fit text-black font-semibold p-[2px] translate-x-1/2">Best Seller</p>
+                        <h2 className="mt-2 text-4xl text-white font-bold">$29</h2>
                         <p className="text-white text-[22px]">STANDARD</p>
                         <ul className="mt-6 text-white/75">
                             <li>Feature 1</li>
@@ -41,7 +42,7 @@ const PricingPage = () => {
                         </ul>
                         <Button className="mt-2 hover:scale-110 transition ease-in-out delay-75 hover:drop-shadow-glow bg-[#bd0e32] text-white font-semibold rounded-[8px] hover:bg-white hover:text-[#222831] duration-200 p-3">Choose Plan</Button>
                     </div>
-                    <div className="hover:scale-110 p-8 border text-center justify-self-center duration-200 backdrop-filter backdrop-blur-md bg-opacity-0 bg-white/0 border-gray-100/15 outline-none shadow-xl rounded-xl">
+                    <div className="hover:scale-110 cursor-pointer p-8 border text-center justify-self-center duration-200 backdrop-filter backdrop-blur-md bg-opacity-0 bg-white/0 border-gray-100/15 outline-none shadow-xl rounded-xl">
                         <h2 className="text-4xl text-white font-bold">$79</h2>
                         <p className="text-white text-[22px]">PREMIUM</p>
                         <ul className="mt-6 text-white/75">
