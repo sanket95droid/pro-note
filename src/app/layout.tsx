53ccc5386a866d3b538c6013a56303b9c335ef2e
sanket95droid <sanketzambre1@gmail.com>
2024-03-01 21:02:17 +0530

    @vercel/speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from '@clerk/nextjs'
 import Provider from "@/components/Provider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" className="bg-[#0e0e0f]">
         <Provider>
-          <body className={inter.className}>{children}</body>
+          <body className={inter.className}>
+            {children}
+            <SpeedInsights />
+          </body>
         </Provider>
       </html>
     </ClerkProvider>
